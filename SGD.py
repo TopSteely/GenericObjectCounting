865f622bf98b84034bba82160712864e6abb9392
TopSteely <tobias.stahl1@gmx.de>
2016-12-11 19:48:33 +0100

    input working

--- SGD.py
+++ SGD.py
@@ -103,7 +103,7 @@ class SGD:
         print len(self.predictor.w)
         level_preds, functions = self.predictor.get_iep_levels(img_data, functions)
         print len(level_preds)
-        print level_preds
+        #print level_preds
         ind_max = level_preds.index(max(level_preds))
         upd, _ = self.learner.iep(img_data, functions[ind_max], ind_max)
         return self.w * upd + self.alpha * self.w, functions
