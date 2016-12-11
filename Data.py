b284f0688279fa7953070e539047c4fdaf6e8c66
TopSteely <tobias.stahl1@gmx.de>
2016-12-11 17:22:46 +0100

    update predictor

--- Data.py
+++ Data.py
@@ -18,6 +18,7 @@ class Data:
         for level in levels:
             sa = surface_area_old(self.tree_boxes, levels[level])
             sa_co = sa/total_size
+            print level, sa_co
             if sa_co != 1.0:
                 self.G.remove_nodes_from(levels[level])
             else:
