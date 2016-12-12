f1250e8df947f6d0d66c74677f63dd5e847b36a8
TopSteely <tobias.stahl1@gmx.de>
2016-12-12 19:43:28 +0100

    debugging SGD

--- Experiment1b.py
+++ Experiment1b.py
@@ -25,7 +25,7 @@ def main():
     scaler = MinMaxScaler()
     training_data = load.training_numbers
     for i_img_nr, img_nr in enumerate(training_data[0:10]):
-        img_data = Data.Data(load, img_nr, tree_level_size)
+        img_data = Data.Data(load, img_nr, tree_level_size, None)
         scaler.partial_fit(img_data.X)
     sgd.set_scaler(scaler)
         
