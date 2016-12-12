0cad93c633df575140c3739fba4c15a5a0e1e6b6
TopSteely <tobias.stahl1@gmx.de>
2016-12-12 20:20:33 +0100

    debugging SGD

--- Experiment1b.py
+++ Experiment1b.py
@@ -17,7 +17,7 @@ def main():
 
     #initialize
     print 'initializing'
-    sgd = SGD.SGD('max', category, tree_level_size, batch_size, math.pow(10,-5), 0.003, 1e-6)
+    sgd = SGD.SGD('max', category, tree_level_size, batch_size, math.pow(10,-3), 0.003, 1e-6)
     load = Input.Input('pascal',category)
     output = Output.Output('pascal_max', category, tree_level_size, '1b')
     
--- SGD.py
+++ SGD.py
@@ -80,14 +80,13 @@ class SGD:
             img_data.scale(self.scaler)
             if img_nr in self.functions:
                 img_functions = self.functions[img_nr]
-                tmp = self.method(img_data, img_functions)
-                self.w_update += tmp[0]
+                self.w_update += self.method(img_data, img_functions)[0]
             else:
                 temp = {}
                 upd, fct = self.method(img_data, temp)
                 self.w_update += upd
                 self.functions[img_nr] = fct
-            
+            self.samples_seen += 1
             
             if i_img_nr%self.batch_size == 0:
                 self.update()
