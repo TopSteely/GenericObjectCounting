3b2550c697c4cb90a5ee217ff4c3be5908539a7b
TopSteely <tobias.stahl1@gmx.de>
2016-12-14 18:18:27 +0100

    predicting 0?

--- SGD.py
+++ SGD.py
@@ -97,7 +97,7 @@ class SGD:
         
     def update(self):
         #print 'updating'
-        self.w -= self.eta * self.w_update
+        self.w += self.eta * self.w_update
         self.eta = self.eta * (1+self.eta0*self.gamma*self.samples_seen)**-1
         self.w_update = np.zeros(self.n_features)
         self.predictor = IEP.IEP(self.w, 'prediction')
