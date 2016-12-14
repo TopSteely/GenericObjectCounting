a7732df311335e7968a74e9ca89efe5da0033bdd
TopSteely <tobias.stahl1@gmx.de>
2016-12-14 18:00:46 +0100

    predicting 0?

--- SGD.py
+++ SGD.py
@@ -111,7 +111,7 @@ class SGD:
         
         #print level_preds
         ind_max = level_preds.index(max(level_preds))
-        upd, _ = self.learner.iep(img_data, functions[ind_max], ind_max)
+        upd, _ = self.learner.iep(img_data, [], ind_max)#functions[ind_max]
         print 'loss: ', self.predict(img_data) - img_data.y
         return (self.predict(img_data) - img_data.y) * upd + self.alpha * self.w, functions
         
