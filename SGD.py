b8d913ee16dd7bd39af29d23fedc8bf7959a7fa5
TopSteely <tobias.stahl1@gmx.de>
2016-12-15 15:15:39 +0100

    learning

--- SGD.py
+++ SGD.py
@@ -88,7 +88,6 @@ class SGD:
                 upd, fct = self.method(img_data, temp)
                 self.w_update += upd
                 self.functions[img_nr] = fct
-            print self.w_update
             self.samples_seen += 1
             if (i_img_nr + 1)%self.batch_size == 0:
                 self.update()
@@ -98,8 +97,7 @@ class SGD:
         
         
     def update(self):
-        print 'updating'
-        self.w -= self.eta * self.w_update
+        self.w -= (self.eta * self.w_update)
         self.eta = self.eta * (1+self.eta0*self.gamma*self.samples_seen)**-1
         self.w_update = np.zeros(self.n_features)
         self.predictor = IEP.IEP(self.w, 'prediction')
