f3ac226a09cf492d19c3b2b020ae645a5315e107
TopSteely <tobias.stahl1@gmx.de>
2016-12-22 19:21:43 +0100

    debugging sgd

--- Experiment1b.py
+++ Experiment1b.py
@@ -17,9 +17,11 @@ def main():
     for tree_level_size in range(1,2):
         #initialize
         print 'initializing'
-        sgd = SGD.SGD('max', category, tree_level_size, batch_size, math.pow(10,-3), 0.003, 0)
+        sgd = SGD.SGD('max', category, tree_level_size, batch_size, math.pow(10,-5), 0.003, 0)
         load = Input.Input('pascal',category)
         output = Output.Output('pascal_max', category, tree_level_size, '1b')
+        
+        print 'debugging, no scaler, plot loss!'
         
         #learn scaler
         scaler = MinMaxScaler()
@@ -27,7 +29,7 @@ def main():
         for i_img_nr, img_nr in enumerate(training_data[0:20]):
             img_data = Data.Data(load, img_nr, tree_level_size, None)
             scaler.partial_fit(img_data.X)
-        sgd.set_scaler(scaler)
+        sgd.set_scaler(None)
             
         # learn SGD
         print 'learning'
