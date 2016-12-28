3eb6352bf0f5b82a2def18fc2f92487724a18040
TopSteely <tobias.stahl1@gmx.de>
2016-12-29 00:34:13 +0100

    trying gamma parameters

--- Experiment1b.py
+++ Experiment1b.py
@@ -37,30 +37,31 @@ def main():
             
         # learn SGD
         print 'learning'
-        for eta_i in [math.pow(10,-3),math.pow(10,-4),math.pow(10,-5),math.pow(10,-6)]:
-            for al_i in [math.pow(10,2),math.pow(10,0),math.pow(10,-2),math.pow(10,-6)]:
-                sgd = SGD.SGD('max', category, tree_level_size, batch_size, eta_i, 0.0003, al_i)
-                sgd.set_scaler(scaler)
-                print al_i, eta_i
-                for epoch in range(15):
-                    sgd.learn(7)
-                    print sgd.evaluate('train',7)
-                sgd_fut = SGDRegressor(eta0=eta_i, learning_rate='invscaling', shuffle=True, average=True, alpha=al_i, n_iter=15)
-                sgd_fut_data = []
-                sgd_fut_y = []
-                for i_img_nr, img_nr in enumerate(training_data[0:7]):
-                    img_data = Data.Data(load, img_nr, tree_level_size, scaler)
-                    sgd_fut_data.append(img_data.X[img_data.levels[0][0]])
-                    sgd_fut_y.append(img_data.y)
-                sgd_fut.fit(sgd_fut_data, sgd_fut_y)
-                sgd_fat_data = []
-                sgd_fat_y = []
-                for i_img_nr, img_nr in enumerate(test_numbers_d[0:7]):
-                    img_data = Data.Data(load, img_nr, tree_level_size, scaler)
-                    sgd_fat_data.append(img_data.X[img_data.levels[0][0]])
-                    sgd_fat_y.append(img_data.y)
-                sgd_a_error = ((sgd_fut.predict(sgd_fat_data) - np.array(sgd_fat_y))**2).sum()
-                print 'scikit GD error: ', sgd_a_error
+        for eta_i in [math.pow(10,-4),math.pow(10,-5)]:
+            for al_i in [math.pow(10,1),math.pow(10,0),math.pow(10,-1),math.pow(10,-2)]:
+                for gamma_i in [math.pow(10,-5),math.pow(10,-4),math.pow(10,-3),math.pow(10,-2)]:
+                    sgd = SGD.SGD('max', category, tree_level_size, batch_size, eta_i, gamma_i, al_i)
+                    sgd.set_scaler(scaler)
+                    print al_i, eta_i, gamma_i
+                    for epoch in range(15):
+                        sgd.learn(7)
+                        print sgd.evaluate('train',7)
+                    sgd_fut = SGDRegressor(eta0=eta_i, learning_rate='invscaling', shuffle=True, average=True, alpha=al_i, n_iter=15)
+                    sgd_fut_data = []
+                    sgd_fut_y = []
+                    for i_img_nr, img_nr in enumerate(training_data[0:7]):
+                        img_data = Data.Data(load, img_nr, tree_level_size, scaler)
+                        sgd_fut_data.append(img_data.X[img_data.levels[0][0]])
+                        sgd_fut_y.append(img_data.y)
+                    sgd_fut.fit(sgd_fut_data, sgd_fut_y)
+                    sgd_fat_data = []
+                    sgd_fat_y = []
+                    for i_img_nr, img_nr in enumerate(test_numbers_d[0:7]):
+                        img_data = Data.Data(load, img_nr, tree_level_size, scaler)
+                        sgd_fat_data.append(img_data.X[img_data.levels[0][0]])
+                        sgd_fat_y.append(img_data.y)
+                    sgd_a_error = ((sgd_fut.predict(sgd_fat_data) - np.array(sgd_fat_y))**2).sum()
+                    print 'scikit GD error: ', sgd_a_error
                 
             
         # evaluate
