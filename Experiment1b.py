749aaacc81927e497e9a9293d16d091d64d92134
TopSteely <tobias.stahl1@gmx.de>
2016-12-29 22:21:39 +0100

    comparing it to dennis features

--- Experiment1b.py
+++ Experiment1b.py
@@ -44,7 +44,7 @@ def main():
             for al_i in [math.pow(10,1),math.pow(10,0),math.pow(10,-1),math.pow(10,-2)]:
                 for gamma_i in [math.pow(10,-5),math.pow(10,-4),math.pow(10,-3),math.pow(10,-2)]:
                     sgd_pascal = SGD.SGD('pascal', 'max', category, tree_level_size, batch_size, eta_i, gamma_i, al_i)
-                    sgd_dennis = SGD.SGD('dennis', 'max', category, tree_level_size, batch_size, eta_i, gamma_i, al_i)
+                    sgd_dennis = SGD.SGD('dennis', 'max', category, tree_level_size, batch_size, eta_i, gamma_i, al_i,4096)
                     sgd_pascal.set_scaler(scaler_pascal)
                     sgd_dennis.set_scaler(scaler_dennis)
                     print al_i, eta_i, gamma_i
