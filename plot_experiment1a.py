58991b3f8a28a3dd3ad92f0fbdc6839f52b15234
TopSteely <tobias.stahl1@gmx.de>
2017-01-17 13:54:26 +0100

    plot exp1a

---
+++ plot_experiment1a.py
@@ -0,0 +1,28 @@
+import matplotlib
+matplotlib.use('agg')
+import pickle
+import matplotlib.pyplot as plt
+
+classes = [
+    "person",
+    "bird", "cat", "cow", "dog", "horse", "sheep",
+    "aeroplane", "bicycle", "boat", "bus", "car", "motorbike", "train",
+    "bottle", "chair", "diningtable", "pottedplant", "sofa", "tvmonitor"
+]
+
+mse_path = "/home/tstahl/plot/1b_dennis_max_mse_%s_%s_%s.p"
+
+for eta in [math.pow(10,-5),math.pow(10,-6)]:
+	depths = []
+	for tree_level_size in range(1,4):
+		print tree_level_size
+		error_tmp = 0.0
+		for class_ in classes:
+			print class_
+			with open(mse_path%(class_,tree_level_size,eta), 'rb') as handle:
+			    mse_tmp = pickle.load(handle)
+			error_tmp += mse_tmp
+		depths.append(error_tmp/len(classes))
+	plt.plot(depths, '-rx', label=eta)
+plt.legend()
+plt.savefig("/home/tstahl/plot/Experiment1a.png")
