3ab2418fd9e4997e49da07353909abd90c1b65de
TopSteely <tobias.stahl1@gmx.de>
2017-01-23 12:57:03 +0000

    comparing sgd vs mlp

---
+++ ExperimentSGDvsMLP.py
@@ -0,0 +1,131 @@
+import sys
+import Input
+import Output
+import Data
+import SGD
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import SGDRegressor
+import math
+import numpy as np
+from sklearn.svm import SVC
+import time
+import random
+
+def main():
+#    if len(sys.argv) != 2:
+#        print 'wrong arguments - call with python Experiment1b.py <category>'
+#        exit()
+    category = sys.argv[1]
+
+    learn_mode = 'category'
+
+    pred_mode = 'mean'
+
+    batch_size = 5
+
+    for tree_level_size in range(1,2):
+        #initialize
+        print 'initializing', tree_level_size
+        #sgd = SGD.SGD('max', category, tree_level_size, batch_size, math.pow(10,-4), 0.003, math.pow(10,-5))
+        #load_pascal = Input.Input('pascal',category)
+        load_dennis = Input.Input('dennis',category)
+        #output_pascal = Output.Output('pascal_max', category, tree_level_size, '1b')
+        output_dennis = Output.Output('dennis_%s'%(pred_mode), category, tree_level_size, 'mlp')
+        
+        #learn scaler
+        #scaler_pascal = StandardScaler()
+        if learn_mode == 'all':
+            training_data = load_dennis.training_numbers
+            test_numbers_d = load_dennis.test_numbers
+            scaler_dennis = load_dennis.get_scaler()
+            if scaler_dennis==[]:
+                print "learning scaler"
+                data_to_scale = []
+                scaler = StandardScaler()
+                print len(training_data)
+                random.shuffle(training_data)
+                for img_nr in training_data[0:400]:
+                     img_data = Data.Data(load_dennis, img_nr, 10, None)
+                     data_to_scale.extend(img_data.X)
+                scaler.fit(data_to_scale)
+                output_dennis.dump_scaler(scaler)
+                scaler_dennis = scaler
+        else:
+            training_data = load_dennis.category_train
+            test_numbers_d = load_dennis.category_val
+            scaler_dennis = load_dennis.get_scaler_category()
+            if scaler_dennis==[]:
+            	print "learning scaler"
+            	data_to_scale = []
+            	scaler_category = StandardScaler()
+            	print len(training_data)
+            	random.shuffle(training_data)
+            	for img_nr in training_data[0:100]:
+            	     img_data = Data.Data(load_dennis, img_nr, 10, None)
+            	     data_to_scale.extend(img_data.X)
+            	scaler_category.fit(data_to_scale)
+            	output_dennis.dump_scaler_category(scaler_category)
+            	scaler_dennis = scaler_category
+            
+        # learn SGD
+        for eta_i in [math.pow(10,-5)]:
+    	    print eta_i
+            for al_i in [0]:#[math.pow(10,-4)]:#,math.pow(10,-2)
+                for gamma_i in [math.pow(10,-5)]:#,math.pow(10,-4),math.pow(10,-3),math.pow(10,-2)
+                    training_loss = []
+                    validation_loss = []
+                    #sgd_pascal = SGD.SGD('pascal', 'max', category, tree_level_size, batch_size, eta_i, gamma_i, al_i)
+                    sgd_dennis = SGD.SGD('dennis', pred_mode, category, tree_level_size, batch_size, eta_i, gamma_i, al_i, 4096)
+                    mlp = MLPRegressor(verbose=True, hidden_layer_sizes=(250,250), learning_rate='invscaling')
+                    #sgd_pascal.set_scaler(scaler_pascal)
+                    sgd_dennis.set_scaler(scaler_dennis)
+                    print al_i, eta_i, gamma_i
+                    for epoch in range(4):
+                        print epoch
+                        #print epoch
+                        #tr_l, te_l = sgd_dennis.learn('categories')
+                        sgd_dennis.learn(learn_mode)
+                        #print tr_l, te_l
+                        
+                        #training_loss.extend(tr_l)
+                        #validation_loss.extend(te_l)
+                        
+                        #print training_loss
+                        #print training_loss, validation_loss
+                        #t1,_,_ = sgd_dennis.evaluate('train', 20)
+                        #t2,_,_ = sgd_dennis.evaluate('val', 20)
+                        #training_loss.append(t1)
+                        #validation_loss.append(t2)
+                        #preds_d_p, preds_skl_p, y_d_p = sgd_pascal.evaluate('train',2, True)
+                        #preds_d_d, preds_skl_d, y_d_d = sgd_dennis.evaluate('train',50, True)
+                        #output_pascal.plot_preds(preds_d_p, preds_skl_p, y_d_p, al_i)
+                        #output_dennis.plot_preds(preds_d_d, preds_skl_d, y_d_d, al_i)
+                    #output_dennis.plot_train_val_loss(training_loss, validation_loss, eta_i, al_i)
+                    mlp_data = []
+                    mlp_y = []
+                    for im_nr in training_data:
+                        img_data = Data.Data(load_dennis, img_nr, 10, None)
+                        mlp_data.append(img_data.X[0])
+                        mlp_y.append(img_data.y)
+                    mlp.fit(mlp_data,mlp_y)
+
+                    mlp_data = []
+                    mlp_y = []
+                    for im_nr in test_numbers_d:
+                        img_data = Data.Data(load_dennis, img_nr, 10, None)
+                        mlp_data.append(img_data.X[0])
+                        mlp_y.append(img_data.y)
+                    preds = mlp.predict(mlp_data)
+
+                    print 'Mlp: ', np.sum(((preds-nmp_y)**2)/len(mlp_y))
+
+                    if learn_mode == 'all':
+                        mse,ae, mse_non_zero = sgd_dennis.evaluate('test')
+                    elif learn_mode == 'category':
+                        mse,ae, mse_non_zero = sgd_dennis.evaluate('val')
+                    print "Eval loss: ", eta_i, al_i, mse
+                    output_dennis.save(mse, ae, mse_non_zero, sgd_dennis, eta_i, al_i, learn_mode)
+    
+    
+if __name__ == "__main__":
+    main()
