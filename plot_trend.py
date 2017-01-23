30ac4dd2345c8f29f0757e0afd7b196dac38c2ba
TopSteely <tobias.stahl1@gmx.de>
2017-01-23 09:25:05 +0000

    finding descending classes

--- plot_trend.py
+++ plot_trend.py
@@ -23,7 +23,7 @@ for class_ in classes:
 			for m_mode in ['max','mean']:
 				if os.path.isfile('/home/tstahl/plot/1b_dennis_%s_mse_%s_%s_%s.p'%(m_mode,class_,1,eta)):
 					print 'exists'
-					previsous = 1000
+					previous = 1000
 					for tree_level_size in range(1,4):
 						if os.path.isfile('/home/tstahl/plot/1b_dennis_%s_mse_%s_%s_%s.p'%(m_mode,class_,tree_level_size,eta)):
 							temp_sting = '%s_%s_%s_%s'%(class_,m_mode,eta,alpha)
@@ -33,14 +33,14 @@ for class_ in classes:
 								print 'test'
 						    	
 						    	print mse_tmp, previous
-						    #if mse_tmp < previsous:
-						    #	if tree_level_size == 2:
-						    #		descending2.append(temp_sting)
-						    #	elif tree_level_size == 3:
-						    #		descending3.append(temp_sting)
-						    #previsous = mse_tmp
-						    #print descending2, descending3
-						    #raw_input()
+							    if mse_tmp < previsous:
+							    	if tree_level_size == 2:
+							    		descending2.append(temp_sting)
+							    	elif tree_level_size == 3:
+							    		descending3.append(temp_sting)
+							    previsous = mse_tmp
+							    print descending2, descending3
+							    raw_input()
 				else:
 					print 'does not exist'
 print descending2
