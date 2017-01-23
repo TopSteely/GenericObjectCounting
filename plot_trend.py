89852947d2b582621a30f6a733b17df160fb67f4
TopSteely <tobias.stahl1@gmx.de>
2017-01-23 09:15:53 +0000

    finding descending classes

--- plot_trend.py
+++ plot_trend.py
@@ -27,9 +27,8 @@ for class_ in classes:
 					for tree_level_size in range(1,4):
 						if os.path.isfile('/home/tstahl/plot/1b_dennis_%s_mse_%s_%s_%s.p'%(m_mode,class_,tree_level_size,eta)):
 							temp_sting = '%s_%s_%s'%(class_,m_mode,eta,alpha)
-
 							with open('/home/tstahl/plot/1b_dennis_%s_mse_%s_%s_%s.p'%(m_mode,class_,tree_level_size,eta), 'rb') as handle:
-						    	mse_tmp = pickle.load(handle)
+						    mse_tmp = pickle.load(handle)
 						    print mse_tmp, previous
 						    if mse_tmp < previsous:
 						    	if tree_level_size == 2:
