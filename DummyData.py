5929e391b5dcf98f537d75132441872d4cb41934
TopSteely <tobias.stahl1@gmx.de>
2017-01-23 15:21:23 +0000

    dummy experiment

--- DummyData.py
+++ DummyData.py
@@ -35,7 +35,7 @@ class DummyData():
 
 		#appending intersections:
 		print self.boxes
-		self.boxes = np.append(self.boxes, np.array([35, 0, 50, 100]), axis=1)
+		self.boxes = np.append(self.boxes, [35, 0, 50, 100], axis=1)
 		print self.boxes
 		self.X = np.append(self.X, [1, 1, 1, 1, 1], axis=0)
 		self.boxes = np.array(self.boxes)
