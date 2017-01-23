3fe96edb4659d88157265c4eedb8b0bdc82118e8
TopSteely <tobias.stahl1@gmx.de>
2017-01-23 14:34:38 +0000

    dummy experiment

--- DummyDataExperiment.py
+++ DummyDataExperiment.py
@@ -12,7 +12,9 @@ import time
 import random
 
 def main():
+	sgd_dummy = SGD.SGD('dennis', 'mean', 'sheep', 3, 5, 0.0001, 0.0001, 0.0001, 5)
 	img_data = DummyData.DummyData()
+	sgd_dummy.method(img_data)
 
 
 if __name__ == "__main__":
