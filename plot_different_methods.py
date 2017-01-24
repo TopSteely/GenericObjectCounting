284f4943cd48a2d772990e89ec9a2d3e3b0dc27b
TopSteely <tobias.stahl1@gmx.de>
2017-01-24 13:08:46 +0000

    plot versions

--- plot_different_methods.py
+++ plot_different_methods.py
@@ -7,7 +7,7 @@ import os
 
 classes = [
     #"person",
-    "cow", "dog", "horse", "sheep",#"bird","cat",
+    "cow",  "horse", "sheep","bird","cat",#"dog"
     "aeroplane", "bicycle", "boat", "bus", "car", "motorbike", "train",
     "bottle", "chair", "diningtable", "pottedplant", "sofa", "tvmonitor"
 ]
