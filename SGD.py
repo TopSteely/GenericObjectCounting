d439e94c0d85c8f4bc049c942601ea720119f2ca
TopSteely <tobias.stahl1@gmx.de>
2017-01-25 10:44:12 +0000

    adding mode multi

--- SGD.py
+++ SGD.py
@@ -228,8 +228,9 @@ class SGD:
             print level_pred, img_data.y
             iep_level, _ = self.learner.iep(img_data, functions, level)
             ret[level,:] = (2 * (level_pred - img_data.y) * iep_level + 2 * self.alpha * self.w_multi[level])
-        print ret, len(ret)
-        raw_input()
+        if img_data.y > 0:
+            print ret, len(ret)
+            raw_input()
         return ret, functions
 
 
