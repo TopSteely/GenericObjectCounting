36d5b6f4671f1e348bd619c356dd348bbf14bb3a
TopSteely <tobias.stahl1@gmx.de>
2017-01-26 20:36:57 +0000

    adding blobexperiment

--- Input.py
+++ Input.py
@@ -232,12 +232,12 @@ class Input:
     def get_features_blob(self, img_nr):
         im = imread('/var/node436/local/tstahl/Dummy/%s.png'%(format(img_nr, "02d")), mode='L')
         #im = cv2.imread('/var/node436/local/tstahl/Dummy/%s.png'%(format(img_nr, "02d")), 0)
-        print im.shape
-        print max(im), min(im)
-        assert np.array_equal(im[:,:,0], im[:,:,1])
-        assert np.array_equal(im[:,:,1], im[:,:,2])
-        im = im[:,:,0]
+        #assert np.array_equal(im[:,:,0], im[:,:,1])
+        #assert np.array_equal(im[:,:,1], im[:,:,2])
+        #im = im[:,:,0]
         #image 6 has to be inverted
+        print im
+        raw_input()
         if img_nr == 6:
             im = 255 - im
         #x = [x0, x1, x2]
