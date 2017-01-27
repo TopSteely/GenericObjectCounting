bbeb378365c64b6e55e7907b64783ba4916878a3
TopSteely <tobias.stahl1@gmx.de>
2017-01-27 17:01:19 +0000

    random boxes blob

--- BlobData.py
+++ BlobData.py
@@ -5,7 +5,11 @@ import time
 class BlobData():
 	def __init__(self, load, img_nr):
 		prune_tree_levels = 10
-		self.boxes = load.get_coords_blob(img_nr)
+		boxes = load.get_coords_blob(img_nr)
+		print boxes[0]
+		print boxes[0][2], boxes[0][3]
+		self.boxes = random_bbox(boxes[0][2], boxes[0][3])
+		print self.boxes
 		self.tree_boxes = self.boxes
 		self.X = load.get_features_blob(img_nr, self.boxes)
 		self.num_features = 3
@@ -24,6 +28,7 @@ class BlobData():
 				self.G.remove_nodes_from(levels[level])
 			else:
 				nr_levels_covered = level
+		raw_input()
 		levels = {k: levels[k] for k in range(0,nr_levels_covered + 1)}
 		# prune levels, speedup + performance 
 		levels_tmp = {k:v for k,v in levels.iteritems() if k<prune_tree_levels}
@@ -33,3 +38,14 @@ class BlobData():
 		for trash_level in levels_gone.values():
 			self.G.remove_nodes_from(trash_level)
 		self.boxes = np.array(self.boxes)
+
+	def random_bbox(im_w, im_h):
+		boxes = [[0,0,im_w,im_h]]
+		for b_i in range(150):
+			box = []
+			box.append(randint(0, im_w))
+			box.append(randint(0, im_h))
+			box.append(randint(box[0], im_w))
+			box.append(randint(box[1], im_h))
+			boxes.append(box)
+		return boxes
