5931a7d1dbfe4b9c4cd67494705ce624c280133d
TopSteely <tobias.stahl1@gmx.de>
2017-01-31 07:38:05 +0000

    plotting loss per samples blob

--- BlobExperiment.py
+++ BlobExperiment.py
@@ -1,3 +1,6 @@
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
 import sys
 import Input
 import Output
@@ -11,31 +14,50 @@ from sklearn.svm import SVC
 import time
 import random
 
-epochs = 3
+epochs = 1
 
 eta = 0.01
 
+pr_mode = 'multi'
+
 def main():
-	for eta in [0.1, 0.01, 0.001, 0.0001]:
-		sgd_blob = SGD.SGD('blob', 'multi', '', 5, 5, eta, 0.0001, 0.1, 3)
-		scaler = StandardScaler()
-		data_to_scale = []
-		for im in sgd_blob.blobtraindata:
-			data_to_scale.extend(im.X)
-		scaler.fit(data_to_scale)
+	for pr_mode in ['mean','multi']:
+		for eta in [0.01]:
+			sgd_blob = SGD.SGD('blob', pr_mode, '', 3, 5, eta, 0.0001, 0.1, 3)
+			scaler = StandardScaler()
+			data_to_scale = []
+			for im in sgd_blob.blobtraindata:
+				data_to_scale.extend(im.X)
+			scaler.fit(data_to_scale)
 
-		#tested
-		for im in sgd_blob.blobtraindata:
-			im.scaler_transform(scaler)
-		for im in sgd_blob.blobtestdata:
-			im.scaler_transform(scaler)
+			#tested
+			for im in sgd_blob.blobtraindata:
+				im.scaler_transform(scaler)
+			for im in sgd_blob.blobtestdata:
+				im.scaler_transform(scaler)
 
-		for ep in range(epochs):
-			sgd_blob.learn()
-			mse,ae, mse_non_zero = sgd_blob.evaluate('blobtest')
-			mse_tr,ae_tr, mse_non_zero_tr = sgd_blob.evaluate('blobtrain')
-			print "Eval loss train: ",eta, mse_tr
-			print "Eval loss val: ",eta, mse
+			for ep in range(epochs):
+				losses_imas_tr = np.array([], dtype=np.int64).reshape(tree_level_size,0)
+				losses_imas_te = np.array([], dtype=np.int64).reshape(tree_level_size,0)
+				for imas in range(1,7):
+					sgd_blob.learn('all',imas)
+					mse,ae, mse_non_zero = sgd_blob.evaluate('blobtest')
+					mse_tr,ae_tr, mse_non_zero_tr = sgd_blob.evaluate('blobtrain')
+					print "Eval loss train: ",eta, mse_tr
+					print "Eval loss val: ",eta, mse
+					if pr_mode == 'multi':
+						losses_imas_tr = np.concatenate((losses_imas_tr,mse_tr), axis=1)#.reshape(-1,1)
+						losses_imas_te = np.concatenate((losses_imas_te,mse), axis=1)#.reshape(-1,1)
+					else:
+						losses_imas_tr.append(mse_tr)
+						losses_imas_te.append(mse)
+			plt.plot(losses_imas_tr, '-rx', label='training error')
+			plt.plot(losses_imas_te, '-g0', label='test error')
+			plt.legend()
+			plt.xlabel('Training  samples')
+			plt.ylabel('MSE')
+			plt.legend('%s %s'%('blob',pr_mode))
+			plt.savefig('/var/node436/local/tstahl/plos/blob_%s'%(pr_mode))
 
 if __name__ == "__main__":
     main()
