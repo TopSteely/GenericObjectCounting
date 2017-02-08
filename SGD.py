9b522cc4f854939010fa6b1bdfd6cdc66cb5b54e
TopSteely <tobias.stahl1@gmx.de>
2017-02-08 08:40:45 +0000

    debugging Dummy1Feat

--- SGD.py
+++ SGD.py
@@ -254,9 +254,9 @@ class SGD:
                 img_data = Data.Data(self.load, img_nr, self.prune_tree_levels, self.scaler, self.n_features, True)
             else:
                 img_data = Data.Data(self.load, img_nr, self.prune_tree_levels, self.scaler, self.n_features)
-            print self.loss_per_level(img_data), self.loss_per_level(img_data).shape
-            tra_loss_temp[0:self.prune_tree_levels] += self.loss_per_level(img_data)
-            tra_loss_temp[self.prune_tree_levels] += self.loss(img_data)
+            print self.loss_per_level(img_data), self.loss_per_level(img_data).shape, tra_loss_temp[0:self.prune_tree_levels,:].shape, tra_loss_temp[0:self.prune_tree_levels].shape
+            tra_loss_temp[0:self.prune_tree_levels,:] += self.loss_per_level(img_data)
+            tra_loss_temp[self.prune_tree_levels,:] += self.loss(img_data)
         for img_nr in validation_ims:
             if self.n_features == 1:
                 img_data = Data.Data(self.load, img_nr, self.prune_tree_levels, self.scaler, self.n_features, True)
@@ -358,6 +358,7 @@ class SGD:
             level_pred, _ = predictor.iep(img_data, [], level)
             iep_level, _ = self.learner.iep(img_data, functions, level)
             print level, iep_level, img_data.y
+            raw_input()
             if len(img_data.levels) >= 10:
                 print level, np.min(iep_level), np.max(iep_level)
             #a = (2 * (level_pred - img_data.y) * iep_level + 2 * self.alpha * self.w_multi[level])
