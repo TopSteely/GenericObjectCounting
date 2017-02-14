bbc77f7fe80751f55ae2faf5a90a8f29f9a9af41
TopSteely <tobias.stahl1@gmx.de>
2017-02-14 12:56:31 +0000

    testing derivatives

--- test_new_loss_derivatives.py
+++ test_new_loss_derivatives.py
@@ -19,11 +19,15 @@ def loss_new_scipy(w, x, y, alpha, level_fct):
     return loss + alpha * math.sqrt(np.dot(w,w))
 
 
+for epoch in range(5):
+	for i_level,level_fct in enumerate(fct.values()):
+	    for fun in level_fct:
+	        update += (np.dot(w, x[fun[1]]) + level_preds[i_level] - img_data.y) * (iep_with_func(w,x,level_fct) + img_data.X[fun[1]])
+	w_update += 2 * update + 2 * alpha * w, fct
 
-for i_level,level_fct in enumerate(fct.values()):
-    for fun in level_fct:
-        update += (np.dot(w, x[fun[1]]) + level_preds[i_level] - img_data.y) * (iep_with_func(w,x,level_fct) + img_data.X[fun[1]])
-w_update += 2 * update + 2 * alpha * w, fct
-
-w -= 0.001 * w_update
-w_update = 0.0
+	w -= 0.001 * w_update
+	w_update = 0.0
+	loss = 0.0
+	for level_fct in fct:
+		loss += loss_new_scipy(w, x, y, alpha, level_fct)
+	print epoch, loss
