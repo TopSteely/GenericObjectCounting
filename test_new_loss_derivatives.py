9db70d855eb8dc6a2f5cbe1ce4798b3f48097326
TopSteely <tobias.stahl1@gmx.de>
2017-02-17 14:42:36 +0000

    added prediction to new loss test

--- test_new_loss_derivatives.py
+++ test_new_loss_derivatives.py
@@ -32,11 +32,9 @@ def loss_new_scipy(w, x, y, alpha, level_fct):
     	iep = iep_with_func(w,x,copy)
         window_pred = np.dot(w, x[fun[1]])
         loss += ((y - iep - window_pred) ** 2)
-        print y, iep + window_pred, loss
     return loss + alpha * math.sqrt(np.dot(w,w))
 
 
-print fct
 for epoch in range(5):
     for i_level,level_fct in enumerate(fct):
     	for fun in level_fct:
@@ -50,9 +48,7 @@ for epoch in range(5):
     loss = 0.0
 
     for level_fct in fct:
-        print loss
     	loss += loss_new_scipy(w, x, y, alpha, level_fct)
-        print level_fct, loss
     print 'Loss', epoch, loss
 
 for i_level,level_fct in enumerate(fct):
