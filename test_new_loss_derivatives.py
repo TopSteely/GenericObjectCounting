e500fbf8b50632ae3c2d96415dafceee9b497f4b
TopSteely <tobias.stahl1@gmx.de>
2017-02-17 13:35:17 +0000

    added prediction to new loss test

--- test_new_loss_derivatives.py
+++ test_new_loss_derivatives.py
@@ -47,11 +47,9 @@ for epoch in range(15):
     		w_update += (np.dot(w, x[fun[1]]) + iep_with_func(w,x,copy) - y) * (iep_with_func(1.0,x,copy) + x[fun[1]])
     w_update += 2 * w_update + 2 * alpha * w
 
-    w -= 0.01 * w_update
+    w -= 0.1 * w_update
     w_update = 0.0
     loss = 0.0
-
-    print fct
 
     for level_fct in fct:
     	loss += loss_new_scipy(w, x, y, alpha, level_fct)
