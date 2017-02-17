8af2c1c307c25954dd868c34a2cbf9c42f33a9b2
TopSteely <tobias.stahl1@gmx.de>
2017-02-17 16:27:09 +0000

    added prediction to new loss test

--- untitled.py
+++ untitled.py
@@ -8,8 +8,8 @@ x = [1.0,0.64,0.36,0.3,0.2]
 y = [1.0,0.5,0.4,-0.1,-0.2]
 alpha = 0
 
-cons = ({'type': 'ineq', 'fun': con1},
-    {'type': 'eq', 'fun': con2})
+#cons = ({'type': 'ineq', 'fun': con1},
+#    {'type': 'eq', 'fun': con2})
 
 def loss_new_scipy(w, x, y, alpha):
     print w
