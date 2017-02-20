d9cdde8f26e273b4640ebe057ab24bc140850434
TopSteely <tobias.stahl1@gmx.de>
2017-02-20 12:32:36 +0000

    constraint 2 features

--- untitled.py
+++ untitled.py
@@ -4,15 +4,15 @@ import math
 from copy import deepcopy
 from scipy.optimize import minimize
 
-x = np.array([[1.0,0.64,0.36,0.3,0.2],[0.1,0.3,0.6]])
+x = np.array([[[1.0,0.5],[0.64,0.32],[0.36,0.18],[0.3,0.15],[0.2,0.1]],[[0.1,0.05],[0.3,0.15],[0.6,0.3]]])
 y = [[-1.0,-0.5,-0.4,-0.1,-0.2],[2.2,3.3,6.6]]
 alpha = 0
 
 def con(w,x,y,alpha):
     ret = 0.0
     for x_ in x:
-    	print w, np.minimum(w*x_-1,0)
-        ret += np.minimum(w*x_-1,0).sum()
+    	print w, np.minimum(np.dot(w,x)_-1,0)
+        ret += np.minimum(np.dot(w,x)_-1,0).sum()
     return ret
 
 cons = ({'type': 'ineq', 'fun': con,'args':(x,y,alpha)})
@@ -24,5 +24,5 @@ def loss_new_scipy(w, x, y, alpha):
             loss += ((y_i - np.dot(w,x_i)) ** 2)
     return loss + alpha * math.sqrt(np.dot(w,w))
 
-res = minimize(loss_new_scipy, np.array([1.0]), args=(x, y, alpha),constraints=cons,method='SLSQP')
+res = minimize(loss_new_scipy, np.array([1.0,2.0]), args=(x, y, alpha),constraints=cons,method='SLSQP')
 print res
