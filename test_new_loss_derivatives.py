6f36714945d3816d210c68b51e0b6272e18d871a
TopSteely <tobias.stahl1@gmx.de>
2017-02-21 08:11:05 +0000

    testing derivative

--- test_new_loss_derivatives.py
+++ test_new_loss_derivatives.py
@@ -4,7 +4,7 @@ import math
 from copy import deepcopy
 from scipy.optimize import minimize
 
-w = 1.0
+w = 0.0
 w_update = 0.0
 x = [[1.0,0.64,0.36,0.3,0.2],[2.0,0.5,1.8,0.3]]
 y = [1.0,2.0]
@@ -54,28 +54,31 @@ def loss_new_scipy(w, x, y, alpha, fct):
                     loss += ((y[img_nr] - iep - window_pred) ** 2)
                 elif fun[0] == '-':
                     loss += ((y[img_nr] - iep + window_pred) ** 2)
-                print level_fct, copy,iep, window_pred, y[img_nr], loss
+                #print level_fct, copy,iep, window_pred, y[img_nr], loss
     return loss + alpha * math.sqrt(np.dot(w,w))
 
-print 'Loss', 'before', w, loss_new_scipy(w, x, y, alpha, fct)
-print 'after'
+#print 'Loss', 'before', w, loss_new_scipy(w, x, y, alpha, fct)
+#print 'after'
 
-#for epoch in range(8):
-#    for img_nr, img_fct in zip(fct.keys(),fct.values()):
-#        for level_fct in img_fct:
-#            for fun in level_fct:
-#                copy = deepcopy(level_fct)
-#                copy.remove(fun)
-#                w_update += (np.dot(w, x[img_nr][fun[1]]) + iep_with_func(w,x[img_nr],copy) - y[img_nr]) * (iep_with_func(1.0,x[img_nr],copy) + x[img_nr][fun[1]])
-#    w_update += 2 * w_update + 2 * alpha * w
+for epoch in range(8):
+    for img_nr, img_fct in zip(fct.keys(),fct.values()):
+        for level_fct in img_fct:
+            for fun in level_fct:
+                copy = deepcopy(level_fct)
+                copy.remove(fun)
+                if fun[0] == '+':
+                    w_update += (np.dot(w, x[img_nr][fun[1]]) + iep_with_func(w,x[img_nr],copy) - y[img_nr]) * (iep_with_func(1.0,x[img_nr],copy) + x[img_nr][fun[1]])
+                elif fun[0] == '-':
+                    w_update += (-np.dot(w, x[img_nr][fun[1]]) + iep_with_func(w,x[img_nr],copy) - y[img_nr]) * (iep_with_func(1.0,x[img_nr],copy) + -x[img_nr][fun[1]])
+    w_update += 2 * w_update + 2 * alpha * w
 
-#    w -= 0.01 * w_update
-#    w_update = 0.0
-#    loss = 0.0
+    w -= 0.01 * w_update
+    w_update = 0.0
+    loss = 0.0
 
-#    print 'Loss', epoch, w, loss_new_scipy(w, x, y, alpha, fct)
-#res = minimize(loss_new_scipy, 1.0, args=(x, y, alpha, fct))
-#print res
+    print 'Loss', epoch, w, loss_new_scipy(w, x, y, alpha, fct)
+res = minimize(loss_new_scipy, 0.0, args=(x, y, alpha, fct))
+print res
 #for i_level,level_fct in enumerate(fct):
 #    ax = predict_new(w, x, y, alpha, level_fct)
 #    print i_level, ax
