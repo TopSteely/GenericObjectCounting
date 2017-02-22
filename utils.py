9f6f78dc4f24da65bbbd8b6336861b27f4d8ec21
TopSteely <tobias.stahl1@gmx.de>
2017-02-22 18:01:25 +0000

    trying with huge alpha

--- utils.py
+++ utils.py
@@ -34,8 +34,9 @@ def loss_new_scipy(w, x, y, alpha, fct):
                     loss += ((y[img_nr] - iep - window_pred) ** 2)
                 elif fun[0] == '-':
                     loss += ((y[img_nr] - iep + window_pred) ** 2)
+        loss+= alpha * math.sqrt(np.dot(w,w))
     print loss, alpha * math.sqrt(np.dot(w,w))
-    return loss + alpha * math.sqrt(np.dot(w,w))
+    return loss #+ alpha * math.sqrt(np.dot(w,w))
 
 
 def iep_with_func(w, X, function):
