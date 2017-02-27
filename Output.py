c8e476c73ec54de4fb140e0ef1394f8e53d6fa87
TopSteely <tobias.stahl1@gmx.de>
2017-02-27 07:53:06 +0000

    just avg, no cm

--- Output.py
+++ Output.py
@@ -188,12 +188,12 @@ class Output:
                 plt.savefig(self.best_path%(self.category,img_nr,lvl,al_i))
                 plt.clf()
                 im_cut = avg_pixls[img_nr][lvl]
-                tmp = plt.cm.jet(im_cut)
-                conv_temp = rgb2gray(tmp)
+                #tmp = plt.cm.jet(im_cut)
+                #conv_temp = rgb2gray(tmp)
                 #print 'max: ', np.max(conv_temp)
                 im_heat = np.zeros((im.shape[0],im.shape[1],4))
                 im_heat[:,:,:3] = im
-                im_heat[:,:,3] = np.rint(255*conv_temp/np.max(conv_temp))
+                im_heat[:,:,3] = np.rint(255*im_cut/np.max(im_cut))
                 print np.unique(im_heat[:,:,3])
                 # colormap * np.max somehow, or npmax-npmin
                 plt.imshow(im_heat, cmap='jet')
