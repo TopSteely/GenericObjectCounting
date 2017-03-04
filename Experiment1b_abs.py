397a872255e24ea4ba8b3e9f4ba5217df37438a4
TopSteely <tobias.stahl1@gmx.de>
2017-03-04 10:31:54 +0000

    testing clipped loss

--- Experiment1b_abs.py
+++ Experiment1b_abs.py
@@ -34,7 +34,7 @@ def main():
 
     eta = math.pow(10,-5)
 
-    for tree_level_size in range(4,7):
+    for tree_level_size in range(2,7):
         #initialize
         print 'initializing', tree_level_size
         #sgd = SGD.SGD('max', category, tree_level_size, batch_size, math.pow(10,-4), 0.003, math.pow(10,-5))
