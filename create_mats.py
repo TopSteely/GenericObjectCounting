17ad3133f6dd7f6321ff3e845668f4b4508aaa22
TopSteely <tobias.stahl1@gmx.de>
2017-03-06 13:49:36 +0000

    creating mats for net

--- create_mats.py
+++ create_mats.py
@@ -34,32 +34,60 @@ def main():
 
     eta = math.pow(10,-4)
 
-    load_dennis = Input.Input('dennis',category,5)
+    load_dennis = Input.Input('dennis',category,20)
     #output_pascal = Output.Output('pascal_max', category, tree_level_size, '1b')
-    output_dennis = Output.Output('dennis_%s'%(pred_mode), category, 5, '1b')
+    output_dennis = Output.Output('dennis_%s'%(pred_mode), category, 20, '1b')
     train_mat = {}
     test_mat = {}
-    im_dict = {}
-    im_dict['image'] = []
-    im_dict['boxes'] = []
-    im_dict['labels'] = []
-    im_dict['functions'] = []
-    for i,img_nr in enumerate(range(1,4)):
-        
+    train_mat['image'] = []
+    train_mat['boxes'] = []
+    train_mat['labels'] = []
+    train_mat['functions'] = []
+    for i,img_nr in enumerate(load_dennis.training_numbers):
+        print img_nr
+        img_data = Data.Data(load_dennis, img_nr, 20, None)
+        # we need: 
+            #'image': '/var/scratch/spintea/Repositories/ms-caffe/data/VOCdevkit2007/VOC2007/JPEGImages/000005.jpg'
+            #'boxes' # (intersections)
+            #labels
+            #functions
+        train_mat['image'].append('/var/scratch/spintea/Repositories/ms-caffe/data/VOCdevkit2007/VOC2007/JPEGImages/%s.jpg'%(format(img_nr, "06d")))
+        train_mat['boxes'].append(img_data.boxes)
+        train_mat['labels'].append([load_dennis.get_all_labels(img_nr)])
+        train_mat['functions'].append(img_data.box_levels)
+        assert len(img_data.box_levels ) == len(img_data.boxes)
+    for i,img_nr in enumerate(load_dennis.val_numbers):
+        print img_nr
         img_data = Data.Data(load_dennis, img_nr, 5, None)
         # we need: 
             #'image': '/var/scratch/spintea/Repositories/ms-caffe/data/VOCdevkit2007/VOC2007/JPEGImages/000005.jpg'
             #'boxes' # (intersections)
             #labels
             #functions
-        im_dict['image'].append('/var/scratch/spintea/Repositories/ms-caffe/data/VOCdevkit2007/VOC2007/JPEGImages/%s.jpg'%(format(img_nr, "06d")))
-        im_dict['boxes'].append(img_data.boxes)
-        im_dict['labels'].append([load_dennis.get_all_labels(img_nr)])
-        im_dict['functions'].append(img_data.box_levels)
+        train_mat['image'].append('/var/scratch/spintea/Repositories/ms-caffe/data/VOCdevkit2007/VOC2007/JPEGImages/%s.jpg'%(format(img_nr, "06d")))
+        train_mat['boxes'].append(img_data.boxes)
+        train_mat['labels'].append([load_dennis.get_all_labels(img_nr)])
+        train_mat['functions'].append(img_data.box_levels)
         assert len(img_data.box_levels ) == len(img_data.boxes)
-        #train_mat.append(im_dict)
-        #train_mat['1'] = im_dict
-    output_dennis.save_mat(im_dict,test_mat)
+
+    test_mat['image'] = []
+    test_mat['boxes'] = []
+    test_mat['labels'] = []
+    test_mat['functions'] = []
+    for i,img_nr in enumerate(load_dennis.test_numbers):
+        print img_nr
+        img_data = Data.Data(load_dennis, img_nr, 5, None)
+        # we need: 
+            #'image': '/var/scratch/spintea/Repositories/ms-caffe/data/VOCdevkit2007/VOC2007/JPEGImages/000005.jpg'
+            #'boxes' # (intersections)
+            #labels
+            #functions
+        test_mat['image'].append('/var/scratch/spintea/Repositories/ms-caffe/data/VOCdevkit2007/VOC2007/JPEGImages/%s.jpg'%(format(img_nr, "06d")))
+        test_mat['boxes'].append(img_data.boxes)
+        test_mat['labels'].append([load_dennis.get_all_labels(img_nr)])
+        test_mat['functions'].append(img_data.box_levels)
+        assert len(img_data.box_levels ) == len(img_data.boxes)
+    output_dennis.save_mat(train_mat,test_mat)
     
     
 if __name__ == "__main__":
