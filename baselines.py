da4f7e11e9ab534a89b67d1c3ee20b5949272094
TopSteely <tobias.stahl1@gmx.de>
2017-03-07 21:03:14 +0000

    baselines nonzero

--- baselines.py
+++ baselines.py
@@ -37,7 +37,7 @@ def main():
     load_dennis = Input.Input('dennis',category,20)
     sum_labels = np.zeros(21)
 
-    for i,img_nr in enumerate(load_dennis.training_numbers[0]):
+    for i,img_nr in enumerate(load_dennis.training_numbers[0:1]):
         img_data = Data.Data(load_dennis, img_nr, 20, None)
         labels = load_dennis.get_all_labels(img_nr)
         sum_labels += labels
