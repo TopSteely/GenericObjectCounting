590db5f59af25e057395d41d68601a4e337359b3
TopSteely <tobias.stahl1@gmx.de>
2017-03-07 21:08:46 +0000

    baselines nonzero

--- baselines.py
+++ baselines.py
@@ -54,7 +54,7 @@ def main():
     error_mean_nn = np.zeros(21)
     error_mean_mean_nn = np.zeros(21)
     occurances = np.zeros(21)
-    for i,img_nr in enumerate(load_dennis.val_numbers[0:55]):
+    for i,img_nr in enumerate(load_dennis.val_numbers[0:155]):
         img_data = Data.Data(load_dennis, img_nr, 20, None)
         labels = load_dennis.get_all_labels(img_nr)
         classes = np.where(labels>0)[0]
@@ -67,7 +67,6 @@ def main():
         error_1 += np.abs(labels - 1)
         error_mean += np.abs(labels - mean_labels)
         error_mean_mean += np.abs(labels - mean_mean_labels)
-        print occurances
     print 'error baseline 0: ', np.mean(error_0[1:]/len(load_dennis.val_numbers)), np.mean(error_0_nn[1:]/occurances[1:])
     print 'error baseline 1: ', np.mean(error_1[1:]/len(load_dennis.val_numbers)), np.mean(error_1_nn[1:]/occurances[1:])
     print error_1_nn[1:]
