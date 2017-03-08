ac57b57985a86d7babee4fc1cf6de7b8b2e1b542
TopSteely <tobias.stahl1@gmx.de>
2017-03-08 11:00:07 +0000

    returned to old create tree

--- utils.py
+++ utils.py
@@ -104,7 +104,7 @@ def create_tree(boxes):
     levels[0] = [0]
     G.add_node(0)
     if len(boxes) != 1:
-        for i, box in zip(boxes[1:len(boxes)], range(1,len(boxes))):
+        for box, i in zip(boxes[1:len(boxes)], range(1,len(boxes))):
             if (box[2]-box[0]) * (box[3]-box[1]) == 0: # some boxes have a surface area of 0 like (0,76,100,76)
                 #print box
                 #print 'surface area of box == 0', i
