d9803782aeea6323e5de4d6403b3721e0ec8bac7
TopSteely <tobias.stahl1@gmx.de>
2017-03-08 09:47:04 +0000

    fixing save_w

--- SGD.py
+++ SGD.py
@@ -321,10 +321,10 @@ class SGD:
             #had to .reshape(-1,) for 'multi', does it work for mean?
             squared_error += img_loss.reshape(-1,)
             error += abs(self.predict(img_data) - img_data.y).reshape(-1,)
+            print self.predict(img_data), img_data.y, error
             if img_data.y > 0:
                 non_zero_error += error
                 n_non_zero += 1
-                print error, non_zero_error, n_non_zero
             if debug:
                 #skl_error += (self.sgd.predict(img_data.X[img_data.levels[0][0]].reshape(1, -1)) - img_data.y)**2
                 if self.version == 'multi':
