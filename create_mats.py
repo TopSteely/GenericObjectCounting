c20ba2f75f12cc776ff7de7814e1746593d2326b
TopSteely <tobias.stahl1@gmx.de>
2017-03-08 18:43:58 +0000

    testing pascal_voc_data

--- create_mats.py
+++ create_mats.py
@@ -64,7 +64,7 @@ def main():
         num_classes = 21
         iep = np.zeros((levels,num_classes))
         patches = img_data.gt_overlaps
-        print patches[0]
+        print patches[552]
         print patches.shape
         for i_lf, lf in enumerate(level_functions):
             if (level_functions[i_lf,0]==1):
