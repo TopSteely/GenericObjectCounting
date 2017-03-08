ef29db4e82d86c3a2f2c8eec2f2d5d44859d9562
TopSteely <tobias.stahl1@gmx.de>
2017-03-08 17:35:12 +0000

    testing pascal_voc_data

--- create_mats.py
+++ create_mats.py
@@ -65,16 +65,17 @@ def main():
         iep = np.zeros((levels,num_classes))
         patches = img_data.gt_overlaps
         print patches[0]
+        print patches.shape
         for level_index in range(levels):
             plus_boxes = np.where((level_functions[:,:]==[1,level_index]).all(axis=1))[0]
             minus_boxes = np.where((level_functions[:,:]==[-1,level_index]).all(axis=1))[0]
             level_iep = np.zeros(21)
             print plus_boxes, minus_boxes
             for c in range(num_classes):
-                level_iep[c] = np.sum(patches[plus_boxes,c,:],axis=0)
+                level_iep[c] = np.sum(patches[plus_boxes,c],axis=0)
                 if len(minus_boxes)>0:
                     #level_iep = np.sum( -1. * np.multiply(patches[minus_boxes],w),axis=0)
-                    level_iep[c] += np.sum(-1 * patches[minus_boxes,c,:],axis=0)
+                    level_iep[c] += np.sum(-1 * patches[minus_boxes,c],axis=0)
             iep[level_index,:] = level_iep
         labels = load_dennis.get_all_labels(img_nr)
         print labels
