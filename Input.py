21d458aba9e6ae0d10eee3747dc80e2225cc5cde
TopSteely <tobias.stahl1@gmx.de>
2017-03-13 23:34:04 +0000

    creat mats

--- Input.py
+++ Input.py
@@ -22,6 +22,11 @@ class Input:
             self.feature_path = 'bla'
             self.test_numbers, training_numbers_tmp = self.get_training_numbers()
             self.training_numbers, self.val_numbers = self.get_val_numbers(training_numbers_tmp)
+            self.classes = ['aeroplane', 'bicycle', 'bird', 'boat',
+           'bottle', 'bus', 'car', 'cat', 'chair',
+           'cow', 'diningtable', 'dog', 'horse',
+           'motorbike', 'person', 'pottedplant',
+            'sheep', 'sofa', 'train', 'tvmonitor']
         elif self.mode == 'mscoco':
             self.coord_path = '/var/node436/local/tstahl/mscoco/SS_Boxes/%s.txt'
             self.label_path = 'bla'
