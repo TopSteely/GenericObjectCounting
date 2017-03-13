0c42b7dd53dd23ed7c318e39967403dff374e6d5
TopSteely <tobias.stahl1@gmx.de>
2017-03-13 15:06:45 +0000

    changes from before images

--- Input.py
+++ Input.py
@@ -346,9 +346,11 @@ class Input:
     def get_grid(self, img_nr):
         feat = []
         for grid in ['12','22','44']:
+            print '/var/node436/local/tstahl/Hard_partitioned_Features/new_Features/%sx%s.txt'%(grid,format(img_nr, "02d"))
             if os.path.isfile('/var/node436/local/tstahl/Hard_partitioned_Features/new_Features/%sx%s.txt'%(grid,format(img_nr, "02d"))):
                 ret = np.loadtxt('/var/node436/local/tstahl/Hard_partitioned_Features/new_Features/%sx%s.txt'%(grid,format(img_nr, "02d")), delimiter=',')
                 feat = np.vstack((feat,ret))
+        print '/var/node436/local/tstahl/Hard_partitioned_Features/%s3x3.txt'%(format(img_nr, "02d"))
         if os.path.isfile('/var/node436/local/tstahl/Hard_partitioned_Features/%s3x3.txt'%(format(img_nr, "02d"))):
             ret = np.loadtxt('/var/node436/local/tstahl/Hard_partitioned_Features/%s3x3.txt'%(format(img_nr, "02d")), delimiter=',')
             feat = np.vstack((feat,ret))
