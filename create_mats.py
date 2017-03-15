108eb225e7e097bde26ecd350adb87b062b4132d
TopSteely <tobias.stahl1@gmx.de>
2017-03-15 18:17:50 +0000

    mscoco test

--- create_mats.py
+++ create_mats.py
@@ -56,113 +56,114 @@ def main():
     print train[0:5], len(train)
     from_ = 81000
     to_ = -1
-    for i,img_nr in enumerate(train[from_:to_]):
-        print i, img_nr
-        if dataset == 'trancos':
-            img_data = Data.Data(load_dennis, img_nr, 20, None, 1)
-        else:
-            img_data = Data.Data(load_dennis, img_nr, level_size, None)
-        # we need: 
-            #'image': '/var/scratch/spintea/Repositories/ms-caffe/data/VOCdevkit2007/VOC2007/JPEGImages/000005.jpg'
-            #'boxes' # (intersections)
-            #labels
-            #functions
-        if img_data.boxes != []:
-            if dataset == 'dennis' or dataset == 'grid' or dataset == 'gt' or dataset == 'sum' or dataset == 'level':
-                train_mat['image'].append('/var/scratch/spintea/Repositories/ms-caffe/data/VOCdevkit2007/VOC2007/JPEGImages/%s.jpg'%(format(img_nr, "06d")))
-            elif dataset == 'mscoco':
-                #print img_nr, load_dennis.get_all_labels(img_nr, 'train'),[load_dennis.classes[i-1] for i in np.where(load_dennis.get_all_labels(img_nr, 'train')>0)[0]]
-                #print len(load_dennis.classes)
-                #print len(load_dennis.get_all_labels(img_nr, 'train'))
-                print load_dennis.get_all_labels(img_nr, 'train').dtype
-                train_mat['image'].append('/var/node436/local/tstahl/mscoco/train2014/%s.jpg'%(format(img_nr, "012d")))
-            elif dataset == 'trancos':
-                train_mat['image'].append('/var/node436/local/tstahl/TRANCOS_v3/1-%s.jpg'%(format(img_nr, "06d")))
-            if level_size == 1:
-                img_data.boxes = [img_data.boxes[0]]
-                img_data.box_levels = [np.array([1,0])]
-            train_mat['boxes'].append(img_data.boxes)
-            
-            #print img_data.boxes
-#            print img_data.box_levels
-#            raw_input()
+    if dataset != 'mscoco':
+        for i,img_nr in enumerate(train[from_:to_]):
+            print i, img_nr
             if dataset == 'trancos':
-                train_mat['labels'].append([load_dennis.get_all_labels(img_nr, 1)])
+                img_data = Data.Data(load_dennis, img_nr, 20, None, 1)
             else:
-                train_mat['labels'].append([load_dennis.get_all_labels(img_nr, 'train')])
-            #print train_mat['labels']
-            #raw_input()
-            if dataset != 'sum':
-
-                train_mat['functions'].append(img_data.box_levels)
-                #train_mat['overlaps'].append(img_data.gt_overlaps)
-                assert len(img_data.box_levels ) == len(img_data.boxes)
-            #print train_mat
-            #raw_input()
-            #test data:
-            #level_functions = np.array(img_data.box_levels)
-            #levels = int(np.amax(level_functions[:,1],axis=0)) + 1
-        #num_classes = 21
-        #iep = np.zeros((levels,num_classes))
-        #print img_data.level_functions[8][0:30]
-        #print img_data.level_functions[9][0:30]
-        #patches = img_data.gt_overlaps
-        #for level_index in range(levels):
-        #    plus_boxes = np.where((level_functions[:,:]==[1,level_index]).all(axis=1))[0]
-        #    minus_boxes = np.where((level_functions[:,:]==[-1,level_index]).all(axis=1))[0]
-        #    level_iep = np.zeros(21)
-        #    for c in range(num_classes):
-        #        level_iep[c] = np.sum(patches[plus_boxes,c],axis=0)
-        #        if len(minus_boxes)>0:
-        #            level_iep[c] += np.sum(-1 * patches[minus_boxes,c],axis=0)
-        #    iep[level_index,:] = level_iep
-        #labels = load_dennis.get_all_labels(img_nr)
-        #for iep_ in iep:
-            #assert np.array_equal(iep_ ,labels)
-    if dataset == 'trancos':
-        for i,img_nr in enumerate(range(1,421)):
-            img_data = Data.Data(load_dennis, img_nr, level_size, None,2)
+                img_data = Data.Data(load_dennis, img_nr, level_size, None)
             # we need: 
                 #'image': '/var/scratch/spintea/Repositories/ms-caffe/data/VOCdevkit2007/VOC2007/JPEGImages/000005.jpg'
                 #'boxes' # (intersections)
                 #labels
                 #functions
             if img_data.boxes != []:
-                train_mat['image'].append('/var/node436/local/tstahl/TRANCOS_v3/2-%s.jpg'%(format(img_nr, "06d")))
+                if dataset == 'dennis' or dataset == 'grid' or dataset == 'gt' or dataset == 'sum' or dataset == 'level':
+                    train_mat['image'].append('/var/scratch/spintea/Repositories/ms-caffe/data/VOCdevkit2007/VOC2007/JPEGImages/%s.jpg'%(format(img_nr, "06d")))
+                elif dataset == 'mscoco':
+                    #print img_nr, load_dennis.get_all_labels(img_nr, 'train'),[load_dennis.classes[i-1] for i in np.where(load_dennis.get_all_labels(img_nr, 'train')>0)[0]]
+                    #print len(load_dennis.classes)
+                    #print len(load_dennis.get_all_labels(img_nr, 'train'))
+                    print load_dennis.get_all_labels(img_nr, 'train').dtype
+                    train_mat['image'].append('/var/node436/local/tstahl/mscoco/train2014/%s.jpg'%(format(img_nr, "012d")))
+                elif dataset == 'trancos':
+                    train_mat['image'].append('/var/node436/local/tstahl/TRANCOS_v3/1-%s.jpg'%(format(img_nr, "06d")))
+                if level_size == 1:
+                    img_data.boxes = [img_data.boxes[0]]
+                    img_data.box_levels = [np.array([1,0])]
                 train_mat['boxes'].append(img_data.boxes)
-                train_mat['labels'].append([load_dennis.get_all_labels(img_nr, 2)])
-                train_mat['functions'].append(img_data.box_levels)
-                #train_mat['overlaps'].append(img_data.gt_overlaps)
-                assert len(img_data.box_levels ) == len(img_data.boxes)
+                
+                #print img_data.boxes
+    #            print img_data.box_levels
+    #            raw_input()
+                if dataset == 'trancos':
+                    train_mat['labels'].append([load_dennis.get_all_labels(img_nr, 1)])
+                else:
+                    train_mat['labels'].append([load_dennis.get_all_labels(img_nr, 'train')])
+                #print train_mat['labels']
+                #raw_input()
+                if dataset != 'sum':
+
+                    train_mat['functions'].append(img_data.box_levels)
+                    #train_mat['overlaps'].append(img_data.gt_overlaps)
+                    assert len(img_data.box_levels ) == len(img_data.boxes)
+                #print train_mat
+                #raw_input()
                 #test data:
-                level_functions = np.array(img_data.box_levels)
-                levels = int(np.amax(level_functions[:,1],axis=0)) + 1
-    if dataset == 'dennis' or dataset == 'grid' or dataset == 'gt' or dataset == 'sum'or dataset == 'level':
-        for i,img_nr in enumerate(load_dennis.val_numbers):
-            print img_nr
-            img_data = Data.Data(load_dennis, img_nr, level_size, None)
-            # we need: 
-                #'image': '/var/scratch/spintea/Repositories/ms-caffe/data/VOCdevkit2007/VOC2007/JPEGImages/000005.jpg'
-                #'boxes' # (intersections)
-                #labels
-                #functions
-            if dataset == 'dennis' or dataset == 'grid' or dataset == 'gt' or dataset == 'sum'or dataset == 'level':
-                train_mat['image'].append('/var/scratch/spintea/Repositories/ms-caffe/data/VOCdevkit2007/VOC2007/JPEGImages/%s.jpg'%(format(img_nr, "06d")))
-            elif dataset == 'mscoco':
-                train_mat['image'].append('/var/node436/local/tstahl/mscoco/train2014/%s.jpg'%(format(img_nr, "012d")))
-            if level_size == 1:
-                img_data.boxes = [img_data.boxes[0]]
-                img_data.box_levels = [np.array([1,0])]
-            #print img_data.boxes
-            #print img_data.box_levels
-            train_mat['boxes'].append(img_data.boxes)
-            train_mat['labels'].append([load_dennis.get_all_labels(img_nr,'train')])
-            
-            #train_mat['overlaps'].append(img_data.gt_overlaps)
-            if dataset != 'sum':
-                train_mat['functions'].append(img_data.box_levels)
-                assert len(img_data.box_levels ) == len(img_data.boxes)
-    output_dennis.save_mat(train_mat,[], dataset, from_,to_)
+                #level_functions = np.array(img_data.box_levels)
+                #levels = int(np.amax(level_functions[:,1],axis=0)) + 1
+            #num_classes = 21
+            #iep = np.zeros((levels,num_classes))
+            #print img_data.level_functions[8][0:30]
+            #print img_data.level_functions[9][0:30]
+            #patches = img_data.gt_overlaps
+            #for level_index in range(levels):
+            #    plus_boxes = np.where((level_functions[:,:]==[1,level_index]).all(axis=1))[0]
+            #    minus_boxes = np.where((level_functions[:,:]==[-1,level_index]).all(axis=1))[0]
+            #    level_iep = np.zeros(21)
+            #    for c in range(num_classes):
+            #        level_iep[c] = np.sum(patches[plus_boxes,c],axis=0)
+            #        if len(minus_boxes)>0:
+            #            level_iep[c] += np.sum(-1 * patches[minus_boxes,c],axis=0)
+            #    iep[level_index,:] = level_iep
+            #labels = load_dennis.get_all_labels(img_nr)
+            #for iep_ in iep:
+                #assert np.array_equal(iep_ ,labels)
+        if dataset == 'trancos':
+            for i,img_nr in enumerate(range(1,421)):
+                img_data = Data.Data(load_dennis, img_nr, level_size, None,2)
+                # we need: 
+                    #'image': '/var/scratch/spintea/Repositories/ms-caffe/data/VOCdevkit2007/VOC2007/JPEGImages/000005.jpg'
+                    #'boxes' # (intersections)
+                    #labels
+                    #functions
+                if img_data.boxes != []:
+                    train_mat['image'].append('/var/node436/local/tstahl/TRANCOS_v3/2-%s.jpg'%(format(img_nr, "06d")))
+                    train_mat['boxes'].append(img_data.boxes)
+                    train_mat['labels'].append([load_dennis.get_all_labels(img_nr, 2)])
+                    train_mat['functions'].append(img_data.box_levels)
+                    #train_mat['overlaps'].append(img_data.gt_overlaps)
+                    assert len(img_data.box_levels ) == len(img_data.boxes)
+                    #test data:
+                    level_functions = np.array(img_data.box_levels)
+                    levels = int(np.amax(level_functions[:,1],axis=0)) + 1
+        if dataset == 'dennis' or dataset == 'grid' or dataset == 'gt' or dataset == 'sum'or dataset == 'level':
+            for i,img_nr in enumerate(load_dennis.val_numbers):
+                print img_nr
+                img_data = Data.Data(load_dennis, img_nr, level_size, None)
+                # we need: 
+                    #'image': '/var/scratch/spintea/Repositories/ms-caffe/data/VOCdevkit2007/VOC2007/JPEGImages/000005.jpg'
+                    #'boxes' # (intersections)
+                    #labels
+                    #functions
+                if dataset == 'dennis' or dataset == 'grid' or dataset == 'gt' or dataset == 'sum'or dataset == 'level':
+                    train_mat['image'].append('/var/scratch/spintea/Repositories/ms-caffe/data/VOCdevkit2007/VOC2007/JPEGImages/%s.jpg'%(format(img_nr, "06d")))
+                elif dataset == 'mscoco':
+                    train_mat['image'].append('/var/node436/local/tstahl/mscoco/train2014/%s.jpg'%(format(img_nr, "012d")))
+                if level_size == 1:
+                    img_data.boxes = [img_data.boxes[0]]
+                    img_data.box_levels = [np.array([1,0])]
+                #print img_data.boxes
+                #print img_data.box_levels
+                train_mat['boxes'].append(img_data.boxes)
+                train_mat['labels'].append([load_dennis.get_all_labels(img_nr,'train')])
+                
+                #train_mat['overlaps'].append(img_data.gt_overlaps)
+                if dataset != 'sum':
+                    train_mat['functions'].append(img_data.box_levels)
+                    assert len(img_data.box_levels ) == len(img_data.boxes)
+        output_dennis.save_mat(train_mat,[], dataset, from_,to_)
     test_mat['image'] = []
     test_mat['boxes'] = []
     test_mat['labels'] = []
@@ -173,6 +174,7 @@ def main():
         test = load_dennis.test_numbers
     elif dataset == 'mscoco':
         test = load_dennis.coco_val_set.getImgIds()
+        print len(test)
     elif dataset == 'trancos':
         test = range(1,422)
 
