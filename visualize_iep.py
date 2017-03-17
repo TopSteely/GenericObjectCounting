15b13e62363122c222ed24dbd9d477b5eff404e8
TopSteely <tobias.stahl1@gmx.de>
2017-03-17 16:11:36 +0000

    coco baseline

--- visualize_iep.py
+++ visualize_iep.py
@@ -30,7 +30,9 @@ for lvl in range(1,iep.shape[0]):
 		plt.axis('off')
 		ax = plt.gca()
 		coord_iep = max_box[i_c,lvl,0:4]
+		print coord_iep
 		coord_iep_ = iep_box[i_c,lvl,0:4]
+		print coord_iep_
 		lvl_pred = iep[lvl,i_c]
 
 		box_ = max_box[i_c,lvl,4]
