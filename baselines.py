d4bba50967ce80b766f18500a2bb15696d9f2724
TopSteely <tobias.stahl1@gmx.de>
2017-06-02 12:57:31 -0700

    changed baselines

--- baselines.py
+++ baselines.py
@@ -58,6 +58,10 @@ def main():
     error_1 = np.zeros(81)
     error_mean = np.zeros(81)
     error_mean_mean = np.zeros(81)
+    error_0_sq = np.zeros(81)
+    error_1_sq = np.zeros(81)
+    error_mean_sq = np.zeros(81)
+    error_mean_mean_sq = np.zeros(81)
     error_0_nn = np.zeros(81)
     error_1_nn = np.zeros(81)
     error_mean_nn = np.zeros(81)
@@ -78,16 +82,27 @@ def main():
         error_mean_mean_nn[classes] += np.abs(labels[classes] - mean_mean_labels)
         occurances[classes] += 1
         error_0 += np.abs(labels)
+        error_0_sq += labels**2
         error_1 += np.abs(labels - 1)
+        error_1_sq += (labels - 1)**2
         error_mean += np.abs(labels - mean_labels)
+        error_mean_sq += (labels - mean_labels)**2
         error_mean_mean += np.abs(labels - mean_mean_labels)
+        error_mean_mean_sq += (labels - mean_mean_labels)**2
     print 'error baseline 0: ', np.mean(error_0[1:]/len(test)), np.mean(error_0_nn[1:]/occurances[1:])
-    print 'mRMSE 0', np.mean(np.sqrt(error_0[1:]/len(test)))
+    print 'mRMSE 0', np.mean(np.sqrt(error_0_sq[1:]/len(test)))
+    print 'mMSE 0', np.mean(error_0_sq[1:]/len(test))
     print 'error baseline 1: ', np.mean(error_1[1:]/len(test)), np.mean(error_1_nn[1:]/occurances[1:])
+    print 'mRMSE 1', np.mean(np.sqrt(error_1_sq[1:]/len(test)))
+    print 'mMSE 1', np.mean(error_1_sq[1:]/len(test))
     print error_1_nn
     print occurances
     print 'error baseline mean: ', np.mean(error_mean[1:]/len(test)), np.mean(error_mean_nn[1:]/occurances[1:])
+    print 'mRMSE mean', np.mean(np.sqrt(error_mean_sq[1:]/len(test)))
+    print 'mMSE mean', np.mean(error_mean_sq[1:]/len(test))
     print 'error baseline mean_mean: ', np.mean(error_mean_mean[1:]/len(test)), np.mean(error_mean_mean_nn[1:]/occurances[1:])
+    print 'mRMSE mean mean', np.mean(np.sqrt(error_mean_mean_sq[1:]/len(test)))
+    print 'mMSE mean mean', np.mean(error_mean_mean_sq[1:]/len(test))
     
     
 if __name__ == "__main__":
